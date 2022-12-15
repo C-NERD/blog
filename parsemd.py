00e1613dcbcf546387e8a14807403243ac333d77
C-NERD <alayaa694@gmail.com>
2022-12-15 18:09:58 +0100

    Initial commit

---
+++ parsemd.py
@@ -0,0 +1,14 @@
+"""
+Python script to convert all markdown articles to html articles.
+
+How to parse all markdown files in the markdown directory into html
+articles :
+    - Look for images with {markdown file name}_logo.png
+    and use those images as the article image.
+
+    - Modify the homepage.html file and add all new
+    articles alongside their images.
+
+    - The article markdown name should be the article
+    name (convert underscores to space).
+"""
